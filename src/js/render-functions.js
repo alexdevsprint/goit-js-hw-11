145ae6f4230398a46c1c93f7a118ab2495c7aa14
alexdevsprint <alexdevsprint@gmail.com>
2025-03-18 16:54:46 +0200

    commit

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,12 +1,23 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+let lightbox;
+
 export function renderPage(data) {
   const gallery = document.querySelector('.gallery');
+  gallery.innerHTML = '';
 
   const galleryMarkup = data.hits
     .map(
-      ({ largeImageURL, webformatURL, tags }) =>
+      ({
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
         `
   <li class="gallery-item">
   <a class="gallery-link" href="${largeImageURL}">
@@ -16,13 +27,23 @@ export function renderPage(data) {
       alt="${tags}"
     />
   </a>
+  <div class="info">
+            <p><b>Likes:</b> ${likes}</p>
+            <p><b>Views:</b> ${views}</p>
+            <p><b>Comments:</b> ${comments}</p>
+            <p><b>Downloads:</b> ${downloads}</p>
+  </div>
 </li>
   `
     )
     .join('');
 
   gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
-  console.log(data.hits);
+
+  lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
 }
 
 export function loader() {}
--- src/main.js
+++ src/main.js
@@ -5,18 +5,40 @@ import getData from './js/pixabay-api';
 import { renderPage } from './js/render-functions';
 
 const form = document.querySelector('.form');
-
+const showLoader = document.querySelector('.loader');
 form.addEventListener('submit', findImage);
 
 function findImage(event) {
   event.preventDefault();
-  const searchName = form.elements['search-text'].value;
-  getData(searchName)
-    .then(data => {
-      renderPage(data);
-    })
-    .catch(error => {
-      console.error(error);
-      s;
+    
+  const searchName = form.elements['search-text'].value.trim();
+
+  if (searchName === '') {
+    iziToast.error({
+      title: 'Error',
+      message: 'You should enter text to search!',
     });
+  } else {
+    showLoader.classList.remove('hidden')  
+    getData(searchName)
+      .then(data => {
+        if (data.hits.length === 0) {
+          iziToast.error({
+            title: 'Error',
+            message:
+              'Sorry, there are no images matching your search query. Please try again!',
+          });
+        } else {
+          renderPage(data);
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      })
+      .finally(() => {
+        form.reset();
+        showLoader.classList.add('hidden');
+      });
+  }
+  
 }
